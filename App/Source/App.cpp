7dd59b36c33729ee48f64191aa0f866a6673eeae
DKFrits <christiaan.gal36@gmail.com>
2025-09-29 19:06:22 +0200

    performance increase?

--- App/Source/App.cpp
+++ App/Source/App.cpp
@@ -1,16 +1,25 @@
 ﻿#include "Util.h"
-#include <format>
 #include <fstream>
-#include <functional>
 #include <iostream>
-#include <iterator>
 #include <string>
-#include <string_view>
 #include <vector>
+
+void SetNewLongestName(int& firstLen, int& lastLen, int& bestLen, std::string& longestFirst, char firstName[512], std::string& longestLast, char  lastName[512], int& field)
+{
+	int totalLen = firstLen + lastLen;
+	if (totalLen > bestLen)
+	{
+		longestFirst.assign(firstName, firstLen);
+		longestLast.assign(lastName, lastLen);
+		bestLen = totalLen;
+	}
+	firstLen = lastLen = 0;
+	field = 0;
+}
 
 int main()
 {
-	std::ifstream file { "C:/Git/data-coding-challenge/App/Source/resources/input-large.csv" };
+	std::ifstream file { "D:/Github/data-coding-challenge/App/Source/resources/input-small.csv" };
 
 	if (!file)
 	{
@@ -20,64 +29,71 @@ int main()
 
 	Timer t;
 
-	std::string longestFirstName;
-	std::string longestLastName;
-
 	const size_t BUFFER_SIZE = 64 * 1024; // 64 KB
 	std::vector<char> buffer(BUFFER_SIZE);
 
-	size_t lineCount = 0;
-	size_t fieldCount = 0;
+	char firstName[512];
+	char lastName[512];
+	int firstLen = 0, lastLen = 0;
+
+	std::string longestFirst;
+	std::string longestLast;
+	int bestLen = 0;
+
 	bool inQuotes = false;
+	int field = 0;
+	bool headerSkipped = false;
 
-	std::vector<char> firstNameBuffer;
-	firstNameBuffer.reserve(256);
+	while (file.read(buffer.data(), buffer.size()) || file.gcount() > 0)
+	{
+		const char* ptr = buffer.data();
+		const char* end = ptr + file.gcount();
 
-	std::vector<char> secondNameBuffer;
-	secondNameBuffer.reserve(256);
+		while (ptr != end)
+		{
+			char ch = *ptr++;
 
-	while (file.read(buffer.data(), buffer.size()) || file.gcount() > 0) {
-		size_t count = file.gcount();
-		for (size_t i = 0; i < count; ++i) {
-			char ch = buffer[i];
-
-			if (lineCount == 0 && ch == '\n') {
-				++lineCount;
-				fieldCount = 0;
+			if (!headerSkipped)
+			{
+				if (ch == '\n') { headerSkipped = true; field = 0; }
 				continue;
 			}
-			if (lineCount == 0) continue;
 
-			if (ch == '"') {
+			if (ch == '"')
+			{
 				inQuotes = !inQuotes;
 			}
-			else if (ch == ',' && !inQuotes) {
-				++fieldCount;
+			else if (ch == ',' && !inQuotes)
+			{
+				++field;
 			}
-			else if (ch == '\n' && !inQuotes) {
-				if (std::ssize(firstNameBuffer) + std::ssize(secondNameBuffer) > std::ssize(longestFirstName) + std::ssize(longestLastName)) {
-					longestFirstName.assign(firstNameBuffer.data(), firstNameBuffer.size());
-					longestLastName.assign(secondNameBuffer.data(), secondNameBuffer.size());
-				}
-
-				firstNameBuffer.clear();
-				secondNameBuffer.clear();
-
-				fieldCount = 0;
+			else if (ch == '\n' && !inQuotes)
+			{
+				SetNewLongestName(firstLen, lastLen, bestLen, longestFirst, firstName, longestLast, lastName, field);
 			}
-			else {
-				if (fieldCount == 2) {
-					firstNameBuffer.push_back(ch);
-				}
+			else
+			{
+				if (field == 2 && firstLen < 511) firstName[firstLen++] = ch;
+				else if (field == 3 && lastLen < 511) lastName[lastLen++] = ch;
+			}
 
-				if (fieldCount == 3) {
-					secondNameBuffer.push_back(ch);
+			if (field > 3 && !inQuotes)
+			{
+				while (ptr != end)
+				{
+					char c = *ptr++;
+					if (c == '"') inQuotes = !inQuotes;
+					else if (c == '\n' && !inQuotes)
+					{
+						SetNewLongestName(firstLen, lastLen, bestLen, longestFirst, firstName, longestLast, lastName, field);
+						break;
+					}
 				}
 			}
 		}
 	}
 
-	std::cout << longestFirstName << " " << longestLastName << "\n";
+	std::cout << longestFirst << " " << longestLast << "\n";
 	std::cout << "Time elapsed: " << t.elapsed() << " seconds\n";
 
 	return 0;
